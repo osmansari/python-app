c80b9bab25c4b4092d2c1ec08cf311cb96a12496
osmansari <94010726+osmansari@users.noreply.github.com>
2021-12-01 20:27:29 +0300

    Update leap-year.py

--- leap-year.py
+++ leap-year.py
@@ -1,8 +1,8 @@
 year=int(input("Enter year to be checked:"))
 if(year%4==0 and year%100!=0 or year%400==0):
-   
+  erttttttttttttttttttttttt
     print("THIS IS JUST A LEAP YEAR!)
-else:
+else: 
     print("The year isn't a leap year!)
           
           
